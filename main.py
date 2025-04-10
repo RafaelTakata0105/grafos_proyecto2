6bb987276db9c5bba4083468cdef557b7a895958
RafaelTakata0105 <rafaeltakata0105@gmail.com>
2025-04-10 14:02:35 -0600

    deleted an extra line

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from faker_netflix import Netflix
-import os
+
 
 netflix = Netflix()
 
 if __name__ == '__main__':
     #Creamos 100 Nodos, si ya se ha corrido una vez es necesario mover el rango de creación de los index
-    netflix.create_nodes(15, 100)
+    netflix.create_nodes(1, 1000)
 
     #Creamos 10000 relaciones de tipo ADD
     netflix.create_relationships(10000)
