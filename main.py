4894f3f9841635322fe2d254b53bc7391925cb38
RafaelTakata0105 <rafaeltakata0105@gmail.com>
2025-04-10 13:58:30 -0600

    100 Nodes and 10000 Rel

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ import os
 netflix = Netflix()
 
 if __name__ == '__main__':
-    netflix.create_nodes(13, 14)
+    #Creamos 100 Nodos, si ya se ha corrido una vez es necesario mover el rango de creación de los index
+    netflix.create_nodes(15, 100)
+
+    #Creamos 10000 relaciones de tipo ADD
+    netflix.create_relationships(10000)
     
